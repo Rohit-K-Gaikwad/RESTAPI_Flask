8b3cfa82e46da00167989e019739341a69c588af
Rohit Gaikwad <rgaikwad1998@gmail.com>
2023-12-23 19:37:46 +0530

    Migrations

--- Tutorial/01-Basic-CRUD-API/practice.py
+++ Tutorial/01-Basic-CRUD-API/practice.py
@@ -4,11 +4,13 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
+
 class HelloWorld(Resource):
     def get(self):
-        return {'key': 'value'}
+        return {"key": "value"}
 
-api.add_resource(HelloWorld, '/vix')
 
-if __name__ == '__main__':
+api.add_resource(HelloWorld, "/vix")
+
+if __name__ == "__main__":
     app.run(debug=True)
--- Tutorial/API-Flask/app.py
+++ Tutorial/API-Flask/app.py
@@ -1,4 +1,3 @@
-
 # https://programminghistorian.org/en/lessons/creating-apis-with-python-and-flask#documentation-and-examples
 
 from flask import Flask, request, render_template, jsonify
@@ -16,48 +15,48 @@ def dict_factory(cursor, row):
     return d
 
 
-@app.route('/', methods=['GET'])
+@app.route("/", methods=["GET"])
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-@app.route('/api/v1/resources/books/all', methods=['GET'])
+@app.route("/api/v1/resources/books/all", methods=["GET"])
 def api_all():
-    conn = sqlite3.connect('src/books.db')
+    conn = sqlite3.connect("src/books.db")
     conn.row_factory = dict_factory
     cur = conn.cursor()
-    all_books = cur.execute('SELECT * FROM books;').fetchall()
+    all_books = cur.execute("SELECT * FROM books;").fetchall()
     return jsonify(all_books)
 
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html')
+    return render_template("404.html")
 
 
-@app.route('/api/v1/resources/books', methods=['GET'])
+@app.route("/api/v1/resources/books", methods=["GET"])
 def api_filter():
     query_parameters = request.args
 
-    published = query_parameters.get('published')
-    author = query_parameters.get('author')
+    published = query_parameters.get("published")
+    author = query_parameters.get("author")
 
     query = "SELECT * FROM books WHERE"
     to_filter = []
 
     if published:
-        query += ' published=? AND'
+        query += " published=? AND"
         to_filter.append(published)
 
     if author:
-        query += ' author=? AND'
+        query += " author=? AND"
         to_filter.append(author)
 
     if not (published or author):
         return page_not_found(404)
-    
-    query = query[:-4] + ';'
-    conn = sqlite3.connect('src/books.db')
+
+    query = query[:-4] + ";"
+    conn = sqlite3.connect("src/books.db")
 
     conn.row_factory = dict_factory
     cur = conn.cursor()
@@ -66,5 +65,5 @@ def api_filter():
     return jsonify(results)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
